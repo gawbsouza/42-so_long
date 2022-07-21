8f0118fa25d0f351127a8a8f482dbba4afb8175f
gabriel <gabriel.souza@zoho.com>
2022-07-21 01:14:09 -0300

    gui_destroy: fix invalid win and mlx pointer

--- src/gui_destroy.c
+++ src/gui_destroy.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 21:02:09 by gasouza           #+#    #+#             */
-/*   Updated: 2022/07/20 21:20:53 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/07/21 01:11:49 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ void	gui_destroy(t_gui **gui)
 {
 	if (!gui || !*gui)
 		return ;
-	mlx_destroy_window((*gui)->mlx, (*gui)->win);
-	mlx_destroy_display((*gui)->mlx);
-	free((*gui)->mlx);
+	if ((*gui)->win && (*gui)->mlx)
+		mlx_destroy_window((*gui)->mlx, (*gui)->win);
+	if ((*gui)->mlx)
+	{
+		mlx_destroy_display((*gui)->mlx);
+		free((*gui)->mlx);
+	}
 	free(*gui);
 	*gui = NULL;
 }
